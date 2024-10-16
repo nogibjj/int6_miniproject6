e7889482cc0548253b84ad6b9ad57d65d8bb7f86
bella-tang <79435936+bella-tang@users.noreply.github.com>
2024-10-16 20:16:09 +0000

    added code for loading data

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,48 +1,50 @@
-import sqlite3
 import os
 import pandas as pd
+from dotenv import load_dotenv
+from databricks import sql
 
-# load the csv file and insert into a new sqlite3 database
+# load the csv file and insert into a new databricks database
 def load(dataset="data/Nutrition.csv"):
-    """Transforms and Loads data into the local SQLite3 database"""
+    """Transforms and Loads data into the databricks database"""
+    payload = pd.read_csv(dataset)
+    load_dotenv()
+    with sql.connect (server_hostname = os.getenv ("SERVER_HOSTNAME"),
+        http_path=os.getenv("HTTP_PATH" ),
+        access_token=os.getenv("DATABRICKS_KEY")
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Nutrition (
+                ID INTEGER PRIMARY KEY,
+                cancer STRING,
+                diabetes STRING,
+                heart_disease STRING,
+                EGGSFREQ INTEGER,
+                GREENSALADFREQ INTEGER,
+                FRIESFREQ INTEGER,
+                MILKFREQ INTEGER,
+                SODAFREQ INTEGER,
+                COFFEEFREQ INTEGER,
+                CAKESFREQ INTEGER
+            )
+            ''')
 
-    # prints the full working directory and path
-    print(f"Current working directory: {os.getcwd()}")
-    df = pd.read_csv(dataset)
+            cursor.execute("SELECT * FROM Nutrition VALUES")
+            check_data = cursor.fetchall()
+            if not check_data:
+                print('Inserting data into Nutrition table...')
+                cursor.executemany('''
+                INSERT INTO Nutrition (
+                    ID, cancer, diabetes, heart_disease, 
+                    EGGSFREQ, GREENSALADFREQ, FRIESFREQ, 
+                    MILKFREQ, SODAFREQ, COFFEEFREQ, CAKESFREQ
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ''', [tuple(row) for row in payload.itertuples(index=False, name=None)])
 
-    # connect to the SQLite database
-    conn = sqlite3.connect('Nutrition.db')
-    c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS Nutrition")
 
-    # create table
-    c.execute('''
-        CREATE TABLE Nutrition (
-            ID INTEGER PRIMARY KEY,
-            cancer TEXT,
-            diabetes TEXT,
-            heart_disease TEXT,
-            EGGSFREQ INTEGER,
-            GREENSALADFREQ INTEGER,
-            FRIESFREQ INTEGER,
-            MILKFREQ INTEGER,
-            SODAFREQ INTEGER,
-            COFFEEFREQ INTEGER,
-            CAKESFREQ INTEGER
-        )
-    ''')
+            cursor.close()
+            connection.close()
 
-    # insert
-    data_to_insert = df.values.tolist()
-    c.executemany('''
-        INSERT INTO Nutrition (
-            ID, cancer, diabetes, heart_disease, 
-                  EGGSFREQ, GREENSALADFREQ, FRIESFREQ, 
-                  MILKFREQ, SODAFREQ, COFFEEFREQ, CAKESFREQ
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', data_to_insert)
 
-    conn.commit()
-    conn.close()
-
-    return "Nutrition.db"
+if __name__ == "__main__":
+    load()
