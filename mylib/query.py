89a72de29b78e076496cc33b911e1db7e4b7ddf0
bella-tang <79435936+bella-tang@users.noreply.github.com>
2024-10-16 20:15:55 +0000

    added complex query

--- mylib/query.py
+++ mylib/query.py
@@ -1,149 +1,54 @@
-import sqlite3
+import os
 from tabulate import tabulate
+from dotenv import load_dotenv
+from databricks import sql
 
-def create_table():
-    """Create the Nutrition table if it doesn't already exist"""
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Nutrition (
-            ID INTEGER PRIMARY KEY,
-            cancer TEXT,
-            diabetes TEXT,
-            heart_disease TEXT,
-            EGGSFREQ INTEGER,
-            GREENSALADFREQ INTEGER,
-            FRIESFREQ INTEGER,
-            MILKFREQ INTEGER,
-            SODAFREQ INTEGER,
-            COFFEEFREQ INTEGER,
-            CAKESFREQ INTEGER
-        )
-    ''')
-    conn.commit()
-    conn.close()
-    print("Table created successfully.")
+complex_query = """
+WITH Nutrition_avg AS(
+  SELECT cancer, diabetes, heart_disease,
+    ROUND(AVG(EGGSFREQ),2) as avg_eggs,
+    ROUND(AVG(GREENSALADFREQ),2) as avg_salad,
+    ROUND(AVG(FRIESFREQ),2) as avg_fries, 
+    ROUND(AVG(MILKFREQ),2) as avg_milk,
+    ROUND(AVG(SODAFREQ),2) as avg_soda,
+    ROUND(AVG(COFFEEFREQ),2) as avg_coffee,
+    ROUND(AVG(CAKESFREQ),2) as avg_cakes
+  FROM Nutrition
+  GROUP BY cancer, diabetes, heart_disease
+)
 
+SELECT ID, n.cancer, n.diabetes, n.heart_disease, 
+  ROUND(n.EGGSFREQ-na.avg_eggs,2) as diff_eggs,
+  ROUND(n.GREENSALADFREQ-na.avg_salad,2) as diff_salad,
+  ROUND(n.FRIESFREQ-na.avg_fries,2) as diff_fries,
+  ROUND(n.MILKFREQ-na.avg_milk,2) as diff_milk,
+  ROUND(n.SODAFREQ-na.avg_soda,2) as diff_soda,
+  ROUND(n.COFFEEFREQ-na.avg_coffee,2) as diff_coffee,
+  ROUND(n.CAKESFREQ-na.avg_cakes,2) as diff_cakes
+FROM Nutrition as n JOIN Nutrition_avg as na 
+  ON n.cancer = na.cancer AND 
+     n.diabetes = na.diabetes AND 
+     n.heart_disease = na.heart_disease
+ORDER BY n.cancer, n.diabetes, n.heart_disease, ID
+"""
 
-def insert_data():
-    """Insert sample data into the Nutrition table"""
-    print('Inserting Data')
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    # Inserting sample rows into the Nutrition table
-    cursor.execute('''
-        INSERT INTO Nutrition (
-                   ID, cancer, diabetes, heart_disease, 
-                   EGGSFREQ, GREENSALADFREQ, FRIESFREQ, 
-                   MILKFREQ, SODAFREQ, COFFEEFREQ, CAKESFREQ)
-        VALUES (1, "Yes", "No", "No", 2, 5, 3, 4, 1, 2, 3)
-    ''')
-    cursor.execute('''
-        INSERT INTO Nutrition (
-                   ID, cancer, diabetes, heart_disease, 
-                   EGGSFREQ, GREENSALADFREQ, FRIESFREQ, 
-                   MILKFREQ, SODAFREQ, COFFEEFREQ, CAKESFREQ)
-        VALUES (2, "No", "Yes", "Yes", 4, 3, 2, 5, 2, 4, 1)
-    ''')
-    conn.commit()
-    conn.close()
-    print("Sample data inserted successfully.")
+def query_nutrition():
+    load_dotenv()
+    with sql.connect (server_hostname = os.getenv ("SERVER_HOSTNAME"),
+        http_path=os.getenv("HTTP_PATH" ),
+        access_token=os.getenv("DATABRICKS_KEY")
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(complex_query)
+            rows = cursor.fetchall()
 
+            headers = ["ID", "cancer", "diabetes", "heart_disease", "diff_eggs", 
+                       "diff_salad", "diff_fries", "diff_milk", "diff_soda", 
+                       "diff_coffee", "diff_cakes"]
+            print(tabulate(rows, headers, tablefmt="grid"))
+            
+            cursor.close()
+            connection.close()
 
-def read_data():
-    """Read and query the database for the top 5 rows of the Nutrition table"""
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Nutrition LIMIT 5")
-    rows = cursor.fetchall()
-    print("Top 5 rows of the Nutrition table:")
-    for row in rows:
-        print(row)
-    conn.close()
-    print()
-
-
-def update_data():
-    """Update a record in the Nutrition table"""
-    print('Updating ID 1 to 6 Eggs')
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    cursor.execute('''
-        UPDATE Nutrition
-        SET EGGSFREQ = 6, cancer = "No"
-        WHERE ID = 1
-    ''')
-    conn.commit()
-    conn.close()
-    print("Record updated successfully.")
-
-
-def delete_data():
-    """Delete a record from the Nutrition table"""
-    print('Deleting Data')
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    cursor.execute('''
-        DELETE FROM Nutrition
-        WHERE ID = 2
-    ''')
-    conn.commit()
-    conn.close()
-    print("Record deleted successfully.")
-
-
-def run_crud_operations():
-    """Run all CRUD operations to demonstrate their functionality"""
-    create_table()
-    read_data()
-
-    insert_data()
-    read_data()
-
-    update_data()
-    read_data()
-
-    delete_data()
-    read_data()
-
-def query_frequent_soda():
-    """Retrieve and print all individuals who frequently consume soda"""
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    
-    cursor.execute('''
-        SELECT ID, SODAFREQ, EGGSFREQ, FRIESFREQ
-        FROM Nutrition
-        WHERE SODAFREQ > 3
-        LIMIT 5
-    ''')
-
-    rows = cursor.fetchall()
-    headers = ["ID", "SODAFREQ", "EGGSFREQ", "FRIESFREQ"]
-    print(tabulate(rows, headers, tablefmt="grid"))
-
-    conn.commit()
-    conn.close()
-    print("Frequent soda drinkers queried successfully.\n")
-
-
-
-def query_heart_disease():
-    """Retrieve and print individuals with heart disease and their food consumption"""
-    conn = sqlite3.connect("Nutrition.db")
-    cursor = conn.cursor()
-    
-    cursor.execute('''
-        SELECT ID, EGGSFREQ, GREENSALADFREQ, FRIESFREQ, SODAFREQ
-        FROM Nutrition
-        WHERE heart_disease = "Yes"
-        LIMIT 5
-    ''')
-    
-    rows = cursor.fetchall()
-    headers = ["ID", "EGGSFREQ", "GREENSALADFREQ", "FRIESFREQ", "SODAFREQ"]
-    print(tabulate(rows, headers, tablefmt="grid"))
-
-    conn.commit()
-    conn.close()
-    print("Heart disease queried successfully.\n")
+if __name__ == "__main__":
+    query_nutrition()
